2203b9bbe7ca64ddd1b2c771aa100fb8a42c2488
Teimur <e7-60#yandex.ru>
2021-11-04 19:03:17 +0300

    ended hw

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import ProductCategory, Product
+from basketapp.models import Basket
 
 
 def index(request):
@@ -20,9 +21,38 @@ def contact(request):
 
 
 def products(request, pk=None):
+
+    title = 'продукты'
+    links_menu = ProductCategory.objects.all()
+
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+        context = {
+            'title': title,
+            'links_menu': links_menu,
+            'category': category,
+            'products': products,
+            'basket': basket,
+        }
+
+        return render(request, 'mainapp/products_list.html', context)
+
+    same_products = Product.objects.all()[3:5]
+
     context = {
-        "links_menu": ProductCategory.objects.all(),
-        'title': 'Продукты'
+        'title': title,
+        'links_menu': links_menu,
+        'same_products': same_products,
 
     }
-    return render(request, 'mainapp/products.html', context=context)
+    return render(request, 'mainapp/products.html', context)
